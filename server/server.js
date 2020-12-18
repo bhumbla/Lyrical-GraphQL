face021016d883f086f664ba3477d8321514a5b3
Aakash Bhumbla <aakash.bhumbla@gmail.com>
2020-12-18 17:35:26 -0600

    REF: updates npm packages to latest, uses environment variables for db connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const express = require('express');
 const models = require('./models');
-const expressGraphQL = require('express-graphql');
+const {graphqlHTTP} = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
@@ -8,19 +11,21 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = `mongodb+srv://${process.env.mongoDBUser}:${process.env.mongoDBPass}@cluster0.mdxmt.mongodb.net/songdb?retryWrites=true&w=majority`;
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
-mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to DB'))
+.catch(error => console.error('Mongo Connection Error', error));
 
 app.use(bodyParser.json());
-app.use('/graphql', expressGraphQL({
+app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }));
